cf94695eaf422849da2f675a4ac5c6b297c3c031
Ryan Kalbach <rpk52@drexel.edu>
2023-01-20 12:56:14 -0500

    init setup HW1

---
+++ HW1/out/server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import * as url from "url";
+let app = express();
+app.use(express.json());
+// create database "connection"
+// use absolute path to avoid this issue
+// https://github.com/TryGhost/node-sqlite3/issues/441
+let __dirname = url.fileURLToPath(new URL("..", import.meta.url));
+let dbfile = `${__dirname}database.db`;
+let db = await open({
+    filename: dbfile,
+    driver: sqlite3.Database,
+});
+await db.get("PRAGMA foreign_keys = ON");
+// res's type limits what responses this request handler can send
+// it must send either an object with a message or an error
+app.get("/foo", (req, res) => {
+    if (!req.query.bar) {
+        return res.status(400).json({ error: "bar is required" });
+    }
+    return res.json({ message: `You sent: ${req.query.bar} in the query` });
+});
+app.post("/foo", (req, res) => {
+    if (!req.body.bar) {
+        return res.status(400).json({ error: "bar is required" });
+    }
+    return res.json({ message: `You sent: ${req.body.bar} in the body` });
+});
+app.delete("/foo", (req, res) => {
+    // etc.
+    res.sendStatus(200);
+});
+//
+// TODO ASYNC/AWAIT EXAMPLE
+//
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+// need async keyword on request handler to use await inside it
+app.get("/bar", async (req, res) => {
+    console.log("Waiting...");
+    // await is equivalent to calling sleep.then(() => { ... })
+    // and putting all the code after this in that func body ^
+    await sleep(3000);
+    // if we omitted the await, all of this code would execute
+    // immediately without waiting for the sleep to finish
+    console.log("Done!");
+    return res.sendStatus(200);
+});
+// test it out! while server is running:
+// curl http://localhost:3000/bar
+// run server
+let port = 3000;
+let host = "localhost";
+let protocol = "http";
+app.listen(port, host, () => {
+    console.log(`${protocol}://${host}:${port}`);
+});
